49170ede00bd10aeebca3fedd37efafdae85fce3
John Stultz <john.stultz@linaro.org>
2014-11-07 11:20:40 -0800

    time: Expose get_monotonic_coarse64() for in-kernel uses
    Adds a timespec64 based get_monotonic_coarse64() implementation
    that can be used as we convert internal users of
    get_monotonic_coarse away from using timespecs.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- include/linux/timekeeping.h
+++ include/linux/timekeeping.h
@@ -25,7 +25,7 @@ struct timespec __current_kernel_time(void);
 /*
  * timespec based interfaces
  */
-struct timespec get_monotonic_coarse(void);
+struct timespec64 get_monotonic_coarse64(void);
 extern void getrawmonotonic64(struct timespec64 *ts);
 extern void ktime_get_ts64(struct timespec64 *ts);
 extern time64_t ktime_get_seconds(void);
@@ -68,6 +68,10 @@ static inline void getrawmonotonic(struct timespec *ts)
 	getrawmonotonic64(ts);
 }
 
+static inline struct timespec get_monotonic_coarse(void)
+{
+	return get_monotonic_coarse64();
+}
 #else
 /**
  * Deprecated. Use do_settimeofday64().
@@ -119,6 +123,11 @@ static inline void getrawmonotonic(struct timespec *ts)
 
 	getrawmonotonic64(&ts64);
 	*ts = timespec64_to_timespec(ts64);
+}
+
+static inline struct timespec get_monotonic_coarse(void)
+{
+	return timespec64_to_timespec(get_monotonic_coarse64());
 }
 #endif
 
